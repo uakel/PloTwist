29a043545cc1b5ad0da8114ee414910105e039bd
Leonard Franz <franzleo@al2.cs.uni-tuebingen.de>
2024-11-20 12:18:53 +0100

    Add comments to example

--- examples/example.py
+++ examples/example.py
@@ -1,16 +1,24 @@
 import numpy as np
 from plotwist import *
 
+# Insert Titles and Subtitles
 title("Example Report")
+# A rule can make your report more readable
 rule()
 subtitle("Embed your Plots")
-with embedded_subplots() as (fig, ax):
+
+# Context Managers for plots that get
+# automatically embedded in the report
+with embedded_subplots(
+    embedding="interactive" # The embedding selects the way the plots are displayed
+) as (fig, ax):
     x = np.linspace(0, 2 * np.pi, 100)
     ax.plot(x, np.sin(x), label='sin(x)')
     ax.plot(x, np.cos(x), label='cos(x)')
     ax.legend()
     ax.grid()
 
+# Sliders are also supported!
 subtitle("Even sliders are supported!")
 with slider_subplots(19) as (figs, axs):
     for i, ax in enumerate(axs):
@@ -22,6 +30,9 @@ with slider_subplots(19) as (figs, axs):
         ax.legend(loc='upper right')
         ax.grid()
 
+# Changing the stacker makes the elements you
+# add to the report appear with a diffent structure
+# and at a different position
 stacker("center")
 subtitle("You can also change the layout of your report")
 from matplotlib import pyplot as plt
@@ -31,7 +42,10 @@ plt.legend()
 plt.grid()
 add_fig() # Plotting without context manager is 
           # also supported
+# Add comments that allow others to understand
+# your plots
 comment("Note the nice standard template!")
 
+# Trigger the report generation
 make()
 
